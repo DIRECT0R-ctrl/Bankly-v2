3f4f009d6e86f0668110b90036a3a7a104dd5028
DIRECT0R-ctrl <DominantVortex@proton.me>
2025-12-15 09:37:49 +0100

    Redirect to login if not logged in

--- includes/auth.php
+++ includes/auth.php
@@ -4,4 +4,11 @@ session_start();
 function is_logged_in() {
 	return isset($_SESSION['usr_id']);
 }
+
+function require_login() {
+	if(!is_logged_in()) {
+		header("Location: /auth/login.php");
+		exit();
+	}
+}
 ?>
