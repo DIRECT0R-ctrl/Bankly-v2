208a8021e26b93e108af6509fb13f0c3bb4b2003
DIRECT0R-ctrl <DominantVortex@proton.me>
2025-12-17 16:52:15 +0100

    simple form (add_client.php)

---
+++ clients/add_client.php
@@ -0,0 +1,6 @@
+<form method='post' action='clients_store.php'>
+	<input type="text" name="full_name" placeholder="Full name" required>
+	<input type="email" name="email" placeholder="Email" required>
+	<input type="text" name="cin" placeholder="CIN" required>
+	<button type="submit"></button>
+</form>
