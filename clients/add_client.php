14e5fd07097f55f2e1ad647dae5b35b5b2cb9624
DIRECT0R-ctrl <DominantVortex@proton.me>
2025-12-18 18:53:26 +0100

    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

--- clients/add_client.php
+++ clients/add_client.php
@@ -1,3 +1,16 @@
+<?php
+
+error_reporting(E_ALL);
+ini_set('display_errors', 1);
+
+
+
+require_once "../includes/auth.php";
+require_login();
+
+?>
+
+<h1>Add Client</h1>
 <form method='post' action='clients_store.php'>
 	<input type="text" name="full_name" placeholder="Full name" required>
 	<input type="email" name="email" placeholder="Email" required>
